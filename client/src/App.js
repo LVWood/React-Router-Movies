b33a23328b9ffd9820b1beab580cc772bb2f6cd2
LVWood <leslee.wood@gmail.com>
2019-05-20 17:46:52 -0500

    some id functionality built out but not rendering correctly. DRY refactoring added

--- client/src/App.js
+++ client/src/App.js
@@ -23,10 +23,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-        <Route path="/" component={MovieList}/>
-        <Route path="/movies/:id" component={Movie} />
+        <SavedList list={this.state.savedList} addToSavedList={this.addToSavedList} />
+        <Route exact path="/" component={MovieList}/>
+        <Route exact path="/movies/:id" component={Movie} />
       </div>
     );
   }
